e3a2f757c30dde22703bc57e88ce6de694f21f74
Keegan Bowen <keegan.bowen@gmail.com>
2015-12-17 21:30:52 -0700

    Update install-net-gargoyle.sh

--- install-net-gargoyle.sh
+++ install-net-gargoyle.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
-cp learner.cfg /etc
-cp learner-watcher.cfg /etc
-cp sorter /usr/local/bin
+#!/bin/sh
+mkdir -p /var/tmp/learner
+touch /var/log/net.log
+touch /var/log/catalog.log
+cp learner.cfg /etc/
+cp learner-watcher.cfg /etc/
+cp sorter /usr/local/bin/
 cp net-gargoyle /usr/local/bin
 chmod +x /usr/local/bin/sorter
 chmod +x /usr/local/bin/net-gargoyle
