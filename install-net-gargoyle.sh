4029c8302bd0ed26856c466e906bb7bc6069e24f
Keegan Bowen <keegan.bowen@gmail.com>
2015-12-17 21:22:06 -0700

    Create install-net-gargoyle.sh

---
+++ install-net-gargoyle.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cp learner.cfg /etc
+cp learner-watcher.cfg /etc
+cp sorter /usr/local/bin
+cp net-gargoyle /usr/local/bin
+chmod +x /usr/local/bin/sorter
+chmod +x /usr/local/bin/net-gargoyle
