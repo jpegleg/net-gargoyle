b50612465ad04e44c7a0cbe38411219f215ebaf1
Keegan Bowen <keegan.bowen@gmail.com>
2016-02-15 23:07:39 -0700

    Update start-net-gargoyle.sh

--- start-net-gargoyle.sh
+++ start-net-gargoyle.sh
@@ -2,7 +2,3 @@
 
 net-gargoyle &
 echo $! > /var/run/net-gargoyle.pid
-
-sec.pl --input=/var/log/net.log --conf=/etc/learner.cfg --log=/var/tmp/learner/learner.log --debug=6 --detach
-
-sec.pl --input=/var/log/new-catalog.log --conf=/etc/learner-watcher.cfg --log=/var/tmp/learner/learner-watcher.log --detach
