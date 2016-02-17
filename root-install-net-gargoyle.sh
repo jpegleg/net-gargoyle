14339671e47d0f53e2edf6fec085b117001d8898
Keegan Bowen <keegan.bowen@gmail.com>
2016-02-16 20:59:13 -0700

    Update root-install-net-gargoyle.sh

--- root-install-net-gargoyle.sh
+++ root-install-net-gargoyle.sh
@@ -1,5 +1,30 @@
 #!/bin/sh
-#
+###################################################################################################
+#                                                                                                 #
+#   Install net-gargoyle, an adhoc network connection catalog and alerting system for Linux       #
+#                                                                                                 #
+###################################################################################################
+# Edit the files and install them as a su would.                                            \
+# Although if you can do this, you can probably install a firewall                            \
+# that is more light weight that does the same thing but better, so... okay.                    \
+###################################################################################################
+#                                                                                                 #
+#   If this script has been run before (.pre-edit files exit) then you can restore them with $1   #
+#                                                                                                 #
+###################################################################################################
+if [ "$1"="restore" ]; then
+  cp sorter.pre-editsorter;
+  cp learner.cfg learner.cfg.pre-edit;
+  cp net-gargoyle.pre-edit net-gargoyle;
+  cp net-gargoyle-sec.pre-edit net-gargoyle-sec;
+else
+  echo "Not restoring .pre-edits...";
+fi
+###################################################################################################
+#                                                                                                 #
+#   Edit the files to use /var/tmp/learner /etc /usr/local/bin and /var/log like a normal admin.  #
+#                                                                                                 #
+###################################################################################################
 if [ $(whoami) -eq root]; then
   echo "Running as root"
 else
@@ -9,8 +34,19 @@ if [ -f ./sorter.pre-edit]; then
   echo "Already backed up sorter.";
 else
   cp sorter sorter.pre-edit;
-  sed -i 's/\=\./\/=\/var\/tmp\/learner/g'
+  cp learner.cfg learner.cfg.pre-edit;
+  cp net-gargoyle net-gargoyle.pre-edit;
+  cp net-gargoyle-sec net-gargoyle-sec.pre-edit;
+  sed -i 's/\=\./\/=\/var\/tmp\/learner/g' sorter
+  sed -i 's/\./\/=\/var\/usr\/local\/bin/g' learner.cfg
+  sed -i 's/\=\./\/=\/var\/tmp\/learner/g' net-gargoyle
+  sed -i 's/\=\./\/=\/var\/tmp\/learner/g' net-gargoyle-sec
 fi
+###################################################################################################
+#                                                                                                 #
+#                     Alright, install the files.                                                 #
+#                                                                                                 #
+###################################################################################################
 chmod +x sorter
 chmod +x ./*sh
 mkdir -p /var/tmp/learner
